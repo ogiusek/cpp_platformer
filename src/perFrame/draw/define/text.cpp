43ed324b10671f644bcc7dc7aeffcfa7037a4ef2
ogius <kowalewski.olgierd@gmail.com>
2023-09-20 09:34:18 +0200

    Added draw text

---
+++ src/perFrame/draw/define/text.cpp
@@ -0,0 +1,26 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+
+using namespace std;
+
+void Draw::Text(GameState *gameState, int x, int y, string text, int fontSize = 32, SDL_Color color = {0, 0, 0})
+{
+  TTF_Font *font = TTF_OpenFont("assets/fonts/font.ttf", fontSize);
+  if (font == NULL)
+  {
+    cout << "Font do not loaded\n";
+    return;
+  }
+
+  SDL_Surface *surface = TTF_RenderText_Solid(font, text.c_str(), color);
+
+  SDL_Texture *texture = SDL_CreateTextureFromSurface(gameState->window.renderer, surface);
+
+  SDL_Rect destRect = {x, y, surface->w, surface->h};
+  SDL_RenderCopy(gameState->window.renderer, texture, NULL, &destRect);
+
+  SDL_FreeSurface(surface);
+  SDL_DestroyTexture(texture);
+  TTF_CloseFont(font);
+}
